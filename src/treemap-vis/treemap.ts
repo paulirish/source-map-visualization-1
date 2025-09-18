5ce91e8d03da1b9a6bd3854b6de3bcb546dafcda
Paul Irish <commits@paul.irish>
2025-09-18 11:58:18 -0700

    got a misallocated area calculation in snippets

--- src/treemap-vis/treemap.ts
+++ src/treemap-vis/treemap.ts
@@ -267,6 +267,8 @@ export let createTreemap = (sourceMapData: SourceMapData, getSplitPct: () => num
       let y2 = Math.round(oy2 + (ny2 - oy2) * t)
       let wrap64 = (x: number) => x - Math.floor(x / 64 - 0.5) * 64
       currentLayout = layoutTreemap([currentNode.node_], x1, y1, x2 - x1, y2 - y1)[0]
+      globalThis.layoutNodes = layoutNodes;
+      globalThis.tree = tree;
       currentOriginX = wrap64(-(ox1 + ox2) / 2) * (1 - t) + (x1 + x2) / 2
       currentOriginY = wrap64(-(oy1 + oy2) / 2) * (1 - t) + (y1 + y2) / 2
     } else {
@@ -290,6 +292,8 @@ export let createTreemap = (sourceMapData: SourceMapData, getSplitPct: () => num
     c.scale(ratio, ratio)
     if (width !== oldWidth || height !== oldHeight) {
       layoutNodes = layoutTreemap(tree.root_.sortedChildren_, 0, 0, width - 1, height - 1)
+      globalThis.layoutNodes = layoutNodes;
+      globalThis.tree = tree;
       updateCurrentLayout()
     }
     draw()
